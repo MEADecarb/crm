a7523c8bc0ff8fd2a674c3d8e813c81f3136b7a9
MEADecarb <152652613+MEADecarb@users.noreply.github.com>
2024-08-23 13:58:49 -0400

    Update home.py
    google signin

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from auth import init_db, login_page, register_page, logout, check_authentication
+from auth import init_db, google_login, logout, check_authentication
 
 # Initialize the database
 init_db()
@@ -8,13 +8,10 @@ init_db()
 def sidebar():
   with st.sidebar:
       if 'authenticated' not in st.session_state or not st.session_state.authenticated:
-          page = st.radio("Go to", ["Login", "Register"])
-          if page == "Login":
-              login_page()
-          elif page == "Register":
-              register_page()
+          if google_login():
+              st.experimental_rerun()
       else:
-          st.write(f"Welcome, {st.session_state.user[1]}!")
+          st.write(f"Welcome, {st.session_state.user[3]}!")
           if st.button("Logout"):
               logout()
               st.experimental_rerun()
@@ -26,8 +23,9 @@ def main_content():
       st.write('Welcome to the MEA Decarb Customer Relationship Management System')
       
       # Your main application code here
-      st.write(f"You are logged in as: {st.session_state.user[1]}")
-      st.write("Your role is:", st.session_state.user[3])
+      st.write(f"You are logged in as: {st.session_state.user[3]}")
+      st.write("Your email is:", st.session_state.user[2])
+      st.write("Your role is:", st.session_state.user[4])
       
       # Add more of your CRM functionality here
 
